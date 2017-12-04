027921cbc93d6a6f918c4112826444caec7f776c
oatnewguo <oatnewguo@gmail.com>
2017-12-03 18:57:06 -0800

    Updated pyspark file and added one to count stops.

---
+++ policedata/la_arrests_spark.py
@@ -0,0 +1,44 @@
+import pyspark
+from pyspark.sql import SQLContext
+
+#open up a spark context
+sc = pyspark.SparkContext()
+# Open an sql context
+sqlContext = pyspark.sql.SQLContext(sc)
+
+#Load in the CSV
+df = sqlContext.read.format("csv").option("header", "true").load("Arrest_Data_from_2010_to_Present.csv")
+
+# List of columns to keep
+keep_list = ['Arrest Date', 'Area ID', 'Sex Code', 'Descent Code']
+
+# Filter out the columns we dont need
+df = df.select([column for column in df.columns if column in keep_list])
+
+#Create a clean CSV with desired columns
+#df.write.csv('mycsv.csv')
+
+descent_codes = list('ABCDFGHIJKLOPSUVWXZ')
+
+#Create a code list of all the codes in the file
+codeList = df.select("Descent Code").rdd.flatMap(lambda x: x).collect()
+api_codes = list('ACDFGJKLPSUVZ')
+
+countW =0
+countB = 0
+countH = 0
+countA = 0
+total = 0
+for p in codeList:
+    if p == 'W':
+        countW +=1
+    elif p == 'B':
+        countB += 1
+    elif p =='H':
+        countH += 1
+    elif p in api_codes:
+        countA += 1
+    total += 1
+
+print("White | Black | Hispanic | Asian | Total")
+print("{:<7} {:<7} {:<7}    {:<7} {}".format(countW, countB, countH, countA, total))
--- policedata/la_stops_spark.py
+++ policedata/la_stops_spark.py
@@ -7,10 +7,10 @@ sc = pyspark.SparkContext()
 sqlContext = pyspark.sql.SQLContext(sc)
 
 #Load in the CSV
-df = sqlContext.read.format("csv").option("header", "true").load("Arrest_Data_from_2010_to_Present.csv")
+df = sqlContext.read.format("csv").option("header", "true").load("Stop_Data_from_2010_to_Present.csv")
 
 # List of columns to keep
-keep_list = ['Arrest Date', 'Area ID', 'Sex Code', 'Descent Code']
+keep_list = ['Stop Date', 'Sex Code', 'Descent Code']
 
 # Filter out the columns we dont need
 df = df.select([column for column in df.columns if column in keep_list])
@@ -22,10 +22,12 @@ descent_codes = list('ABCDFGHIJKLOPSUVWXZ')
 
 #Create a code list of all the codes in the file
 codeList = df.select("Descent Code").rdd.flatMap(lambda x: x).collect()
+api_codes = list('ACDFGJKLPSUVZ')
 
 countW =0
 countB = 0
 countH = 0
+countA = 0
 total = 0
 for p in codeList:
     if p == 'W':
@@ -34,7 +36,9 @@ for p in codeList:
         countB += 1
     elif p =='H':
         countH += 1
+    elif p in api_codes:
+        countA += 1
     total += 1
 
-print("White | Black | Hispanic")
-print("{:<6}  {:<6}  {:<6}".format(countW, countB, countH))
+print("White | Black | Hispanic | Asian | Total")
+print("{:<7} {:<7} {:<7}    {:<7} {}".format(countW, countB, countH, countA, total))
